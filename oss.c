f1aae31d6df579c9d074b79fe37b892a932fe5d0
Erosh Boralugodage <erosh.bc@gmail.com>
2023-09-09 03:59:21 -0500

    help menu added

--- oss.c
+++ oss.c
@@ -2,6 +2,7 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 int main(int argc, char **argv){
     /*parse options and recieve command line arguments.
@@ -9,13 +10,34 @@ int main(int argc, char **argv){
     loops runs untill getopt() returns -1
     switch statement value of options to determine options passed
     if unknown option passed exit from the program*/
+ 
+    int opt_val = 0;
 
-    int n=0, s=0 , t=0, opt=0;
+	while((opt_val = getopt(argc, argv, "n:hf:e")) != -1){
+		switch (opt_val){
+	
+		case 'h':
+			printf("oss [-h] [-n proc] [-s simul] [-t iter]\n\n");
+            printf("proc - number of total children to launch.\n");
+            printf("simul- how many children to reun simulataneously.\n");
+            printf("iter -- number of pass to the worker process\n");
+            printf("\n\n");
+			break;
+		case 'f':
+			printf("This is in f \n");
+			break;
+		case 'e':
+			printf("This is on e\n");
+			break;
+		default:
+			return 1;
+
+		}
+    }
+     
     
+
     
-
-
-
     /*
     printf("This is from oss.c\n\n");
 
